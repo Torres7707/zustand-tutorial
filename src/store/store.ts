7a71dc54bbd8b267b94b5847cace1f743960f92c
Torres7707 <wangdong7707@gmail.com>
2024-11-23 21:19:07 +0800

    presist

--- src/store/store.ts
+++ src/store/store.ts
@@ -4,15 +4,20 @@ import { Store } from "@/types/store";
 import { createUserSlice } from "./userSlice";
 import { immer } from "zustand/middleware/immer";
 import { createCartSlice } from "./cartSlice";
-import { devtools, subscribeWithSelector } from "zustand/middleware";
+import { devtools, persist, subscribeWithSelector } from "zustand/middleware";
 
 export const useStore = create<Store>()(
 	devtools(
-		subscribeWithSelector(
-			immer((...a) => ({
-				...createUserSlice(...a), // 显式传递 set、get 和 store
-				...createCartSlice(...a), // 显式传递 set、get 和 store
-			}))
+		persist(
+			subscribeWithSelector(
+				immer((...a) => ({
+					...createUserSlice(...a), // 显式传递 set、get 和 store
+					...createCartSlice(...a), // 显式传递 set、get 和 store
+				}))
+			),
+			{
+				name: "local-storage",
+			}
 		)
 	)
 );
